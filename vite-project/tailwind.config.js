3281ce52110b8613d6b6c39635dce93d95e1b46e
Pratik Raje <105652654+pratik247-02@users.noreply.github.com>
2025-02-26 22:58:14 +0530

    Retarded ThemeChanger

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    darkMode: 'class', // Enables dark mode using the 'class' strategy
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
